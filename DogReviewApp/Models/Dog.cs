03d9876df6e14e6ef9c294c1d0d25e6dec4de53e
Idris.Osinfade <osinfadeidris@gmail.com>
2023-01-12 10:03:24 +0100

    Add project files.

---
+++ DogReviewApp/Models/Dog.cs
@@ -0,0 +1,12 @@
+﻿namespace DogReviewApp.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }   
+        public ICollection<DogOwner> DogOwners { get; set; }
+        public ICollection<DogCategory> DogCategories { get; set; }
+    }
+}
---
+++ DogReviewApp/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace DogReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Dog Dog  { get; set; }
+
+    }
+}
